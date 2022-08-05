88e0796ed509069e9502fc13414294118f1e5441
Jesus Rodriguez <jerodriguezgar1@outlook.com>
2022-08-05 17:24:32 -0500

    did some kbo team analysis

--- src/03_team_analysis.R
+++ src/03_team_analysis.R
@@ -1,33 +1,39 @@
-#this will be some R analyis using the korean baseball league dataset
+#analysis on league teams
 
-head(kbobattingdata)
-
-skim(kbobattingdata)
 
 #get the number of years each team has been in the league
+#create dataframe that stores the number of years each has played in the league
 years_played <- kbobattingdata %>% 
   group_by(team) %>% 
-  summarise(years_in_league=n())
+  summarise(years_total=n())
+save(years_played,file = "years_played.rda")
 
 #plot the number of years played by each team
-ggplot(years_played,aes(x=team,y=years_in_league,fill=team))+
+ggplot(years_played,aes(x=team,y=years_total,fill=team))+
   geom_col()+
   scale_fill_viridis(discrete = T,alpha=.8,option="A")+
   coord_flip()+
-  theme_minimal()+
   labs(title = "Years each team has played in the league",
        x="Team",y="Number of Years")+
   guides(fill="none")
-  
-#total number of games played in kbo by year
+ggsave("years_played.png")
+
+#total number of games played in the kbo by year
+#create df 
 games_played <- kbobattingdata %>% 
   group_by(year) %>% 
   tally(games) %>% 
   mutate(games_total=n) %>% 
-  select(year,games_total) %>% 
-  ggplot(aes(x=year,y=games_total,fill=games_total))+
+  select(year,games_total)
+save(games_played,file = "games_played.rda")
+
+#plot the number of games played in the kbo by year
+ggplot(games_played,aes(x=year,y=games_total,fill=games_total))+
   geom_col()+
   scale_fill_viridis(discrete = F,alpha=.8,option="A")+
   labs(title = "Total Number of Games Played in KBO",
        subtitle = "by Year",
-       x="Year",y="Number of Games")
+       x="Year",y="Number of Games")+
+  guides(fill=guide_legend(title="Scale"))
+ggsave("games_played.png")
+
