cbed83e253b0f35f4cbf5dad6130bde3b6f466ac
Jesus Rodriguez <jerodriguezgar1@outlook.com>
2022-08-05 19:51:54 -0500

    more graphs more fun

--- src/03_team_analysis.R
+++ src/03_team_analysis.R
@@ -37,3 +37,37 @@ ggplot(games_played,aes(x=year,y=games_total,fill=games_total))+
   guides(fill=guide_legend(title="Scale"))
 ggsave("games_played.png")
 
+#batter age df
+batter_age <- kbobattingdata %>% 
+  group_by(year,team) %>% 
+  select(year,team,average_batter_age)
+save(batter_age,file="batter_age.rda")
+  
+#scatter plot for batter age
+ggplot(batter_age,aes(x=year,y=average_batter_age))+
+  geom_point(aes(color=average_batter_age))+
+  scale_color_viridis(discrete = F,option = "C",guide=guide_legend(title="Average Batter Age"))+
+  labs(title = "Average Batter Age",
+       subtitle = "by Year",
+       x="Year",y="Average Age")
+ggsave("batter_age_scatter.png")
+
+#plot facet wrap
+ggplot(batter_age,aes(x=year,y=average_batter_age))+
+  geom_point(aes(color=average_batter_age))+
+  scale_color_viridis(discrete = F,option = "F",guide=guide_legend(title="Average Batter Age"))+
+  labs(title = "Average Batter Age",
+       subtitle = "by Year, by Team",
+       x="Year",y="Average Age")+
+  facet_wrap(~team)
+ggsave("batter_age_scatter_facet_teams.png")
+
+#plot bar graph
+ggplot(batter_age,aes(x=year,y=average_batter_age,fill=average_batter_age))+
+  geom_col()+
+  scale_fill_viridis(discrete = F,option = "A",guide=guide_legend(title="Average Batter Age"))+
+  labs(title = "Average Batter Age",
+       subtitle = "by Year",
+       x="Year",y="Average Age")
+ggsave("average_batter_age_bar.png")
+
